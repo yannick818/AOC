671cb16c9c9bf325c34e5213c3aeb528f61c8353
yannick818 <yannick.stnr+github@gmail.com>
2023-12-08 14:05:25 +0100

    Day 3 improvements

--- src/day/d3_gear_ratios.rs
+++ src/day/d3_gear_ratios.rs
@@ -6,19 +6,19 @@ use crate::prelude::*;
 #[test]
 fn test_gear_ratios() {
     let input = "467..114..
-        ...*......
-        ..35..633.
-        ......#...
-        617*......
-        .....+.58.
-        ..592.....
-        ......755.
-        ...$.*....
-        .664.598..";
-
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
     assert_eq!(4361, cal_gear_ratio(input).unwrap());
 }
 
+#[derive(Debug)]
 struct Number {
     value: u32,
     x: usize,
@@ -34,13 +34,32 @@ impl Number {
         }
     }
 
-    fn touches_symbol(&self, symbols: &[Symbol]) -> bool {
+    fn touches_symbol(&self, symbols: &[Symbol], input: &str) -> bool {
         let x_range = (self.x.saturating_sub(1))..=(self.x+1);
-        let y_range = (self.y.start.saturating_sub(1))..=(self.y.end+1);
+        let y_range = (self.y.start.saturating_sub(1))..(self.y.end+1);
 
-        symbols.iter().any(|symbol| {
-            matches!((x_range.contains(&symbol.x), y_range.contains(&symbol.y)), (true, true))
+        let is_touching = symbols.iter().any(|symbol| {
+            x_range.contains(&symbol.x) && y_range.contains(&symbol.y)
+        });
+
+        // println!("{} {:#?}", is_touching, self);
+        // Print Area for visualisation
+        let area = input.lines().enumerate()
+        .filter_map(|(x, line)| {
+            if x_range.contains(&x) {
+                Some(line)
+            } else {
+                None
+            }
         })
+        .map(|line| {
+            &line[y_range.clone()]
+        })
+        .collect::<Vec<_>>();
+        
+        eprintln!("{} {:#?}", is_touching, area);
+
+        is_touching
         
     }
 }
@@ -56,7 +75,7 @@ pub fn cal_gear_ratio(input: &str) -> Result<u32> {
     let (numbers, symbols) = transform_input(input)?;
 
     let sum = numbers.into_iter().filter_map(|number| {
-        if number.touches_symbol(&symbols) {
+        if number.touches_symbol(&symbols, input) {
             Some(number.value)
         } else {
             None
@@ -71,23 +90,33 @@ fn transform_input(input: &str) -> Result<(Vec<Number>, Vec<Symbol>)> {
     let mut numbers = Vec::new();
     let mut symbols = Vec::new();
 
-    input.lines().enumerate().for_each(|(x, line)| {
-        let elements = line.split('.').filter(|str| !str.is_empty());
-        for element in elements {
-            match element.parse::<u32>() {
-                Ok(value) => {
-                    let str = value.to_string();
-                    let pos = line.find(&str).unwrap();
-                    let len = str.len();
-                    let number = Number::new(value, x, pos..(pos+len));
+    input.lines().map(|line| line.trim()).enumerate().for_each(|(x, line)| {
+        // println!("Line: {line}");
+        let mut num_buffer = String::new();
+        line.chars().enumerate().for_each(|(y, char)| {
+            let mut save_num = || {
+
+                if !num_buffer.is_empty() {
+                    let value = num_buffer.parse::<u32>().unwrap();
+                    let len = num_buffer.len();
+                    let number = Number::new(value, x, (y-len)..(y));
                     numbers.push(number);
+                    num_buffer.clear();
                 }
-                Err(_) => {
-                    let y = line.find(element).unwrap();
+            };
+            match (char, char.is_numeric()) {
+                ('.', _)  => {
+                    save_num();
+                },
+                (digit, true) => {
+                    num_buffer.push(digit);
+                },
+                (_symbol, false) => {
                     symbols.push(Symbol{x, y});
-                }
+                    save_num();
+                },
             }
-        }
+        });
     });
 
     Ok((numbers, symbols))
