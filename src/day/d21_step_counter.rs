c55967b976b12ba96e66cd8c4baa153fd10e06dd
yannick818 <yannick.stnr+github@gmail.com>
2023-12-27 13:22:50 +0100

    tried Day 21.2 with sets...

--- src/day/d21_step_counter.rs
+++ src/day/d21_step_counter.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::prelude::*;
 
@@ -21,10 +21,12 @@ fn test_plot_count() {
 }
 
 #[test]
+#[allow(unreachable_code)]
 fn test_plot_count_extending() {
     assert_eq!(cal_plot_count_extending(INPUT, 6).unwrap(), 16);
     assert_eq!(cal_plot_count_extending(INPUT, 10).unwrap(), 50);
     assert_eq!(cal_plot_count_extending(INPUT, 50).unwrap(), 1594);
+    todo!("takes too long");
     assert_eq!(cal_plot_count_extending(INPUT, 100).unwrap(), 6536);
     assert_eq!(cal_plot_count_extending(INPUT, 500).unwrap(), 167004);
     assert_eq!(cal_plot_count_extending(INPUT, 1000).unwrap(), 668697);
@@ -40,17 +42,16 @@ fn test_modulo() {
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Position(isize, isize);
-type Seated = bool;
 
 #[derive(Clone, Copy)]
 enum Field {
     Rock,
-    Garden(Seated),
-    Start(Seated),
+    Garden,
+    Start,
 }
 
 struct Map {
-    fields: HashMap<Position, Field>,
+    fields: HashMap<Position, (HashSet<Position>, Field)>,
     init_rows: isize,
     init_cols: isize,
 }
@@ -61,26 +62,8 @@ impl Map {
             // find positions of occupied seats and reset them
             let seats = self
                 .fields
-                .iter_mut()
-                .filter_map(|(pos, field)| match field {
-                    Field::Garden(seated) => {
-                        if *seated {
-                            *seated = false;
-                            Some(*pos)
-                        } else {
-                            None
-                        }
-                    }
-                    Field::Start(seated) => {
-                        if *seated {
-                            *seated = false;
-                            Some(*pos)
-                        } else {
-                            None
-                        }
-                    }
-                    _ => None,
-                })
+                .values_mut()
+                .flat_map(|(set, _)| set.drain())
                 .collect::<Vec<_>>();
 
             // place new seats
@@ -95,33 +78,31 @@ impl Map {
                     ]
                 })
                 .for_each(|pos| {
-                    // self.fields.
-                    if let Some(field) = self.fields.get_mut(&pos) {
+                    if let Some((set, field)) = self.fields.get_mut(&pos) {
                         match field {
-                            Field::Garden(seat) => *seat = true,
-                            Field::Start(seat) => *seat = true,
-                            _ => (),
-                        }
+                            Field::Garden => set.insert(pos),
+                            Field::Start => set.insert(pos),
+                            _ => true,
+                        };
                     } else if extending {
                         // % performs remainder, not modulo
-                        let init_pos = Position(pos.0.rem_euclid(self.init_rows), pos.1.rem_euclid(self.init_cols));
-                        let mut field = *self.fields.get(&init_pos).unwrap();
-                        match &mut field {
-                            Field::Garden(seat) => *seat = true,
-                            Field::Start(seat) => *seat = true,
-                            _ => (),
-                        }
-                        self.fields.insert(pos, field);
+                        let init_pos = Position(
+                            pos.0.rem_euclid(self.init_rows),
+                            pos.1.rem_euclid(self.init_cols),
+                        );
+                        let (set, field) = self.fields.get_mut(&init_pos).unwrap();
+                        match field {
+                            Field::Garden => set.insert(pos),
+                            Field::Start => set.insert(pos),
+                            _ => true,
+                        };
                     }
                 })
         }
     }
 
     fn count_seated(&self) -> usize {
-        self.fields
-            .iter()
-            .filter(|(_, field)| matches!(field, Field::Garden(true) | Field::Start(true)))
-            .count()
+        self.fields.values().map(|(set, _)| set.len()).sum()
     }
 
     fn new(input: &str) -> Self {
@@ -129,11 +110,26 @@ impl Map {
             .lines()
             .enumerate()
             .flat_map(|(row, line)| {
-                line.chars().enumerate().map(move |(col, c)| match c {
-                    '#' => (Position(row as isize, col as isize), Field::Rock),
-                    '.' => (Position(row as isize, col as isize), Field::Garden(false)),
-                    'S' => (Position(row as isize, col as isize), Field::Start(true)),
-                    _ => panic!("invalid input {}", c),
+                line.chars().enumerate().map(move |(col, c)| {
+                    let pos = Position(row as isize, col as isize);
+
+                    let typ = match c {
+                        '#' => Field::Rock,
+                        '.' => Field::Garden,
+                        'S' => Field::Start,
+                        _ => panic!("invalid input {}", c),
+                    };
+
+                    let set = match typ {
+                        Field::Start => {
+                            let mut set = HashSet::new();
+                            set.insert(pos);
+                            set
+                        }
+                        _ => HashSet::new(),
+                    };
+
+                    (pos, (set, typ))
                 })
             })
             .collect();
@@ -152,6 +148,8 @@ pub fn cal_plot_count(input: &str, steps: usize) -> Result<usize> {
     Ok(map.count_seated())
 }
 
+// TODO impl Day 21.2 faster
+#[allow(dead_code)]
 pub fn cal_plot_count_extending(input: &str, steps: usize) -> Result<usize> {
     let mut map = Map::new(input);
     map.step(steps, true);
