b186a56ec4777a91746f66d34cf6860b2a524195
yannick818 <yannick.stnr+github@gmail.com>
2023-12-31 03:19:14 +0100

    Day 23.2 slow used queues instead of recursion

--- src/day/d23_a_long_walk.rs
+++ src/day/d23_a_long_walk.rs
@@ -40,6 +40,34 @@ fn test_longest_hike_noslope() {
     assert_eq!(cal_longest_hike_noslope(INPUT).unwrap(), 154);
 }
 
+#[test]
+fn test_4way() {
+    let input = "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.#...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#######.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#";
+
+    assert_eq!(cal_longest_hike_noslope(input).unwrap(), 118);
+}
 type Walked = bool;
 
 #[derive(Sequence, Copy, Clone, PartialEq, Eq)]
@@ -89,7 +117,6 @@ struct Trail {
     trail: Array2D<Tile>,
     walk_pos: Position,
     dead_pos: Position,
-    prev_dead_pos: Position,
     end: Position,
     path_len: usize,
 }
@@ -156,140 +183,168 @@ impl Trail {
             end: Position(rows.len() - 1, end),
             path_len: 0,
             dead_pos: Position(0, start),
-            prev_dead_pos: Position(0, start),
         }
     }
 
     fn get_hike_len(mut self, ignore_slope: bool) -> Vec<usize> {
-        let mut path_lens = Vec::new();
         *self
             .trail
             .get_mut(self.walk_pos.0, self.walk_pos.1)
             .unwrap() = Tile::Path(true);
-        self.hike(&mut path_lens, ignore_slope)
+        self.hike(ignore_slope)
     }
 
-    fn hike(&self, path_lens: &mut Vec<usize>, ignore_slope: bool) -> Vec<usize> {
-        match self.walk(ignore_slope) {
-            WalkResult::End(path_len) => path_lens.push(path_len),
-            WalkResult::Walked(paths) => {
-                for path in paths {
-                    path.hike(path_lens, ignore_slope);
-                }
+    fn hike(self, ignore_slope: bool) -> Vec<usize> {
+        let mut path_lens = Vec::new();
+        let mut queue = vec![self];
+        while let Some(trail) = queue.pop() {
+            println!("queue len {}", queue.len());
+            println!("results len {}", path_lens.len());
+            match trail.walk(ignore_slope) {
+                WalkResult::End(path_len) => path_lens.push(path_len),
+                WalkResult::Walked(mut paths) => queue.append(&mut paths),
             }
         }
-        path_lens.clone()
+        path_lens
     }
 
-    fn walk(&self, ignore_slope: bool) -> WalkResult {
+    fn walk(self, ignore_slope: bool) -> WalkResult {
         static mut GOAL: usize = 0;
         static mut DEAD_END: usize = 0;
         // println!("{:?}", self);
-        if self.walk_pos == self.end {
-            unsafe {
-                GOAL += 1;
+        let mut queue = vec![self];
+        while let Some(trail) = queue.pop() {
+            if trail.walk_pos == trail.end {
+                unsafe {
+                    GOAL += 1;
+                }
+                let ratio = unsafe { GOAL as f64 / DEAD_END as f64 };
+                println!("ratio {}", ratio);
+                return WalkResult::End(trail.path_len);
             }
-            return WalkResult::End(self.path_len);
-        }
 
-        let mut new_trails = enum_iterator::all::<Direction>()
-            .filter_map(|dir| self.walk_pos.walk(dir).map(|pos| (dir, pos)))
-            .filter_map(|(walk_dir, pos)| {
-                let tile = self.trail.get(pos.0, pos.1).unwrap();
-                match tile {
-                    Tile::Forest => None,
-                    Tile::Path(true) => None,
-                    Tile::Path(false) => {
-                        let mut new_trail = self.clone();
-                        *new_trail.trail.get_mut(pos.0, pos.1).unwrap() = Tile::Path(true);
-                        new_trail.dead_pos = new_trail.walk_pos;
-                        new_trail.walk_pos = pos;
-                        new_trail.path_len += 1;
-                        Some(new_trail)
-                    }
-                    Tile::Slope(true, _) => None,
-                    Tile::Slope(false, slope_dir) => {
-                        if walk_dir == *slope_dir || ignore_slope {
-                            let mut new_trail = self.clone();
-                            *new_trail.trail.get_mut(pos.0, pos.1).unwrap() =
-                                Tile::Slope(true, *slope_dir);
+            let mut new_trails = enum_iterator::all::<Direction>()
+                .filter_map(|dir| trail.walk_pos.walk(dir).map(|pos| (dir, pos)))
+                .filter_map(|(walk_dir, pos)| {
+                    let tile = trail.trail.get(pos.0, pos.1).unwrap();
+                    match tile {
+                        Tile::Forest => None,
+                        Tile::Path(true) => None,
+                        Tile::Path(false) => {
+                            let mut new_trail = trail.clone();
+                            *new_trail.trail.get_mut(pos.0, pos.1).unwrap() = Tile::Path(true);
                             new_trail.dead_pos = new_trail.walk_pos;
                             new_trail.walk_pos = pos;
                             new_trail.path_len += 1;
                             Some(new_trail)
-                        } else {
-                            None
+                        }
+                        Tile::Slope(true, _) => None,
+                        Tile::Slope(false, slope_dir) => {
+                            if walk_dir == *slope_dir || ignore_slope {
+                                let mut new_trail = trail.clone();
+                                *new_trail.trail.get_mut(pos.0, pos.1).unwrap() =
+                                    Tile::Slope(true, *slope_dir);
+                                new_trail.dead_pos = new_trail.walk_pos;
+                                new_trail.walk_pos = pos;
+                                new_trail.path_len += 1;
+                                Some(new_trail)
+                            } else {
+                                None
+                            }
                         }
                     }
+                })
+                .collect::<Vec<_>>();
+
+            match new_trails.len() {
+                0 => {
+                    // println!("DEAD END");
+                    // println!("{:?}", self);
+                    unsafe {
+                        DEAD_END += 1;
+                    }
+                    let ratio = unsafe { GOAL as f64 / DEAD_END as f64 };
+                    println!("ratio {}", ratio);
+                    return WalkResult::Walked(Vec::new());
                 }
-            })
-            .collect::<Vec<_>>();
-
-        if new_trails.len() > 1 {
-            new_trails = new_trails
-                .into_iter()
-                .filter_map(|trail| trail.walk_dead_end(ignore_slope))
-                .collect();
-        }
-
-        if new_trails.is_empty() {
-            unsafe {
-                DEAD_END += 1;
+                // finish iterating simple path
+                1 => {
+                    queue.push(new_trails.pop().unwrap());
+                }
+                // split path
+                _ => {
+                    let new_trails = new_trails
+                        .into_iter()
+                        .filter_map(|trail| trail.walk_dead_end())
+                        .collect::<Vec<_>>();
+                    if new_trails.len() == 1 {
+                        queue.push(new_trails.into_iter().next().unwrap());
+                    } else {
+                        return WalkResult::Walked(new_trails);
+                    }
+                }
             }
-            let ratio = unsafe { GOAL as f64 / DEAD_END as f64 };
-            println!("ratio {}", ratio);
         }
-
-        WalkResult::Walked(new_trails)
+        unreachable!()
     }
 
-    fn walk_dead_end(mut self, ignore_slope: bool) -> Option<Self> {
+    fn walk_dead_end(mut self) -> Option<Self> {
         let mut walked = false;
-        loop {
+        let mut queue = vec![self.dead_pos];
+        while let Some(pos) = queue.pop() {
+            self.dead_pos = pos;
             // println!("{:?}", self);
             if self.dead_pos == self.end {
-                break None;
+                // println!("DEAD END");
+                // println!("{:?}", self);
+                return None;
             }
             let possible_ways = enum_iterator::all::<Direction>()
-                .filter_map(|dir| self.dead_pos.walk(dir).map(|pos| (dir, pos)))
-                .filter(|(walk_dir, pos)| {
+                .filter_map(|dir| self.dead_pos.walk(dir))
+                .filter(|pos| {
                     let tile = self.trail.get(pos.0, pos.1).unwrap();
                     match tile {
                         Tile::Forest => false,
                         Tile::Path(true) => false,
                         Tile::Path(false) => true,
                         Tile::Slope(true, _) => false,
-                        Tile::Slope(false, slope_dir) => true,
+                        Tile::Slope(false, _) => true,
                     }
                 })
-                .map(|(_, pos)| pos)
                 .collect::<Vec<_>>();
 
-            if possible_ways.len() == 1 {
-                let pos = possible_ways[0];
-                let tile = self.trail.get_mut(pos.0, pos.1).unwrap();
-                self.prev_dead_pos = self.dead_pos;
-                self.dead_pos = pos;
-                match tile {
-                    Tile::Path(false) => {
-                        *tile = Tile::Forest;
+            //check with cur pos O or #
+            let cur_is_path = matches!(
+                self.trail.get(self.dead_pos.0, self.dead_pos.1).unwrap(),
+                Tile::Path(true) | Tile::Slope(true, _)
+            );
+
+            match (possible_ways.len(), cur_is_path) {
+                (1, _) | (2, true) => {
+                    for pos in possible_ways {
+                        queue.push(pos);
+                        *self.trail.get_mut(pos.0, pos.1).unwrap() = Tile::Forest;
+                        self.dead_pos = pos;
                     }
-                    Tile::Slope(false, _) => {
-                        *tile = Tile::Forest;
+                    walked = true;
+                }
+                (0, _) => {}
+                _ => {
+                    if walked {
+                        // step back
+                        let prev_tile = self
+                            .trail
+                            .get_mut(self.dead_pos.0, self.dead_pos.1)
+                            .unwrap();
+                        *prev_tile = Tile::Path(false);
                     }
-                    _ => panic!("invalid tile"),
                 }
-                walked = true;
-            } else {
-                if walked {
-                    let prev_tile = self
-                        .trail
-                        .get_mut(self.dead_pos.0, self.dead_pos.1)
-                        .unwrap();
-                    *prev_tile = Tile::Path(false);
-                }
-                break Some(self);
             }
+        }
+        if walked {
+            Some(self)
+        } else {
+            None
         }
     }
 }
@@ -303,6 +358,7 @@ pub fn cal_longest_hike(input: &str) -> Result<usize> {
 
 pub fn cal_longest_hike_noslope(input: &str) -> Result<usize> {
     let trail = Trail::new(input);
-    let longest = trail.get_hike_len(true).into_iter().max().unwrap();
+    let hikes = trail.get_hike_len(true);
+    let longest = hikes.into_iter().max().unwrap();
     Ok(longest)
 }
